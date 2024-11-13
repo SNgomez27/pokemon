a8cedde6e3b677a63572c4fd49135a680a05d132
Santiago Nicolas Gomez <gomezsantus@gmail.com>
2024-11-13 19:47:00 +0100

    actividad que mando para casa, para mostrar foto y descripcion del pokemon

--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -1,25 +1,39 @@
 import {Component, OnInit} from '@angular/core';
 import {PokemonApi} from '../services/interfaces/pokimons';
 import {PokemonDetailService} from '../services/pokemon/pokemon-detail.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-pokemon-detail',
   templateUrl: './pokemon-detail.component.html',
   styleUrl: './pokemon-detail.component.scss'
 })
-export class PokemonDetailComponent implements OnInit{
-  pokemon: PokemonApi| null={
-    name:"",
-    url:""
+export class PokemonDetailComponent implements OnInit {
+  pokemon: PokemonApi | null = {
+    name: "",
+    url: ""
   }
+  pokemonDetalles: any
+
   constructor(
-    private pokemonDetailsService: PokemonDetailService
+    private pokemonDetailsService: PokemonDetailService,
+    private http: HttpClient
   ) {
   }
+
   ngOnInit() {
-    this.pokemonDetailsService.infoApi$.subscribe(pk =>{
+    this.pokemonDetailsService.infoApi$.subscribe(pk => {
       this.pokemon = pk
     })
+    this.getAllPokemonData()
   }
 
+  getAllPokemonData() {
+    this.http.get<any>('https://pokeapi.co/api/v2/pokemon/' + this.pokemon?.name).subscribe(datos => {
+      console.log(datos)
+      this.pokemonDetalles = datos
+    })
+
+
+  }
 }
